c6ebafc0652334e1579a738f9a2d1f8329aa0a76
Javed <jvdali.e@gmail.com>
2025-02-14 20:35:33 +0400

    project structure created

---
+++ data_dump.py
@@ -0,0 +1,28 @@
+import pymongo 
+import pandas as pd
+import json , os , sys
+import logging
+
+#mongodb connection url , taken from mongodb
+uri = "mongodb+srv://jvdaliMDB:12345@cluster0.cqzzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# creating a mongodb client object to connect to the server
+client = pymongo.MongoClient(uri)
+
+
+DATA_FILE_PATH = r"D:\DS-AB\Project\MLOps_Pro_Insurance\Insurance-Premium-Prediction-Project\insurance_data.csv"  # data file path in local machine , to dump in mongodb
+DATABASE_NAME = "Insurance_Data"          # variable for mongodb database name 
+COLLECTION_NAME = "Data_Collection"  # variable for mongodb collection name, records are known as collections in mongodb
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)              #reading data from local machine using panda library
+    print(f"Rows and columns: {df.shape}")        #checking shape of the data by printing on terminal ,prior inseting into mongodb
+
+    df.reset_index(drop = True, inplace = True)   #dropping index of the dataset permanently(True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])                         #checking 1st collection of the dataset by printing on terminal ,after transformation into collection is done
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)  
+
+    #client[DATABASE_NAME][COLLECTION_NAME] accesses the specific database and collection in MongoDB.
+    #inserting tranformed dataset into mongodb to insert multiple collections into MongoDB
---
+++ template.py
@@ -0,0 +1,56 @@
+import os, sys
+from pathlib import Path
+import logging
+
+#asking project name 
+while True:
+    project_name = input("Enter the project name: ")
+    if project_name!= "":                                 #if project name not entered then keep asking
+        break                                             #if project name is entered, break the loop and proceed
+
+
+#list of all necessary folders & files
+List_of_files = [
+    #folders & files inside project folder
+     ".github/workflows/.gitkeep",                        # to protect credentials/files from being uploaded to github
+    f"{project_name}/__init__.py",                        # for converting project folder into a package
+    f"{project_name}/components/__init__.py"
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/config.py",
+  
+
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/utils/__init__.py",
+
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/exception/__init__.py",
+
+    f"{project_name}/predictor/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+
+    #folders & files outside project folder
+    f"configs/config.yaml",
+    "requirements.txt",
+    "setup.py",
+    "main.py"
+]
+
+#List of all necessary folders & files to be created
+for filepath in List_of_files: 
+    filepath = Path(filepath)        #It provides cross-platform compatibility, automatically handling path separators (like / on Unix and \ on Windows).
+    filedir,filename = os.path.split(filepath)  #It returns a tuple with these two components: (filedir, filename)
+
+
+    #below if statement to create folders
+    if filedir != "":
+        #os.mkdir()  : only creates the last folder in the path. Intermediate folders must already exist.
+        #os.makedirs() : To create an entire folder structure(folders & file)
+        os.makedirs(filedir, exist_ok=True)                 #create folder if that folder doesnt exist(True)
+
+    #below if statement to create files
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):    # if file size is zero overwrite else skip
+        with open (filepath, "w") as f:
+            pass                            # w is used to open the file in write mode and pass/leave it
+            logging.info(f"Creating empty file:{filepath}")
+    else:
+            logging.info(f"{filename} already exists)")
